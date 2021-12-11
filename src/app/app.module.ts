043942db4bb7d4497450ca92f3174ed4705e8172
Ahassnat <alhassnatabdallah@gmail.com>
2021-12-12 01:03:15 +0200

    Inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { StatusBar } from '@capacitor/status-bar';
+import {BluetoothSerial} from '@ionic-native/bluetooth-serial/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Tab1PageModule } from './tab1/tab1.module';
+import { Tab1Page } from './tab1/tab1.page';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BluetoothSerial],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule {
+
+}
